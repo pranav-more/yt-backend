c127c4b935cbe429ae2fc0f7cfbacd49fedfbd7b
pranav more <pranavmore.psm@gmail.com>
2024-06-04 22:50:30 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -31,4 +31,9 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// --------- Routes Import ----------
+import userRouter from "./routes/user.routes.js";
+
+// --------- Routes Declaration ----------
+app.use("/api/v1/users", userRouter);
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "/.env",
@@ -16,7 +17,7 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.log("mongo db connect fail !!!", err);
+    console.log("mongo db connect fail.......!!!", err);
   });
 
 /*
